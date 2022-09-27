cf66aa17901451d47ddae60bf1f622fe696072fb
Cassandra Byrge <byrgeca1@gmail.com>
2022-09-27 11:54:00 -0400

    completed task 4

--- index.js
+++ index.js
@@ -112,10 +112,15 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
-
+function getInningScore(functionCB) {
+ 
+ return {
+  Home: functionCB(),
+  Away: functionCB()
+ }
 }
+
+console.log(getInningScore(inning));
 
 
 /* STRETCH: ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
